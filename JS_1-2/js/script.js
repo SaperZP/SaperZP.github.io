0dbcede52f557e639cec743ce6d7dd45c1034bf5
Andriy Bilous <belous.a.o@gmail.com>
2017-05-16 12:25:43 +0300

    v.1

---
+++ JS_1-2/js/script.js
@@ -0,0 +1,38 @@
+(function () {
+    "use strict";
+    var number = prompt('enter the number'),
+        power = prompt('enter the math power');
+
+
+    function pow(givenNumber, givenPower) {
+        var result = givenNumber;
+
+        for (var i = 1; i < givenPower; i++) {
+            result *= givenNumber;
+        }
+        return result;
+    }
+
+    console.log(pow(number, power));
+})();
+
+
+(function () {
+    "use strict";
+    var namesArr = [1, 2, 3, 4, 5];
+    for (var i = 0; i < 5; i++) {
+        namesArr[i] = prompt('set the name N: ' + (i + 1) + 'out of 5');
+
+    }
+
+    var user_name = prompt('enter your name');
+
+    for (i = 0; i < namesArr.length; i++) {
+        if (namesArr[i] === user_name) {
+            alert(user_name + ' access granted!');
+        } else {
+            alert('access denied!!!');
+        }
+    }
+
+})();
