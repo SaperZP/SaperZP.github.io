bad980f72b77c2b45645931f5a73779b9e9f9c58
Andriy Bilous <belous.a.o@gmail.com>
2017-06-02 11:12:07 +0300

    v.2

--- JS_3-4/js/script.js
+++ JS_3-4/js/script.js
@@ -7,15 +7,16 @@
                 questions: [
                     {
                         title: 'Вопрос #1',
-                        answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3']
+                        answers: ['Вариант овтета 1.1', 'Вариант овтета 1.2', 'Вариант овтета 1.3']
                     },
                     {
                         title: 'Вопрос #2',
-                        answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3', 'Вариант овтета 4']
+                        answers: ['Вариант овтета 2.1', 'Вариант овтета 2.2', 'Вариант овтета 2.3', 'Вариант овтета' +
+                        ' 2.4']
                     },
                     {
                         title: 'Вопрос #3',
-                        answers: ['Вариант овтета 1', 'Вариант овтета 2']
+                        answers: ['Вариант овтета 3.1', 'Вариант овтета 3.2']
                     }
                 ]
             },
@@ -28,59 +29,67 @@
             },
             form: function () {
                 var form = this.gimmeBlock('form', 'container'),
-                    button = this.gimmeBlock('input', 'btn btn-default'),
                     header = this.gimmeBlock('header', 'row'),
                     headerDiv = this.gimmeBlock('div', 'page-header col-lg-4 col-lg-offset-4'),
                     headerH1 = this.gimmeBlock('h1'),
-                    headerText = document.createTextNode(this.data.title);
+                    headerText = document.createTextNode(this.data.title),
+                    button = this.gimmeBlock('input', 'btn btn-default');
+                /*-----------------^vars^-----------------------*/
 
+                /*header building*/
                 headerH1.appendChild(headerText);
                 headerH1.style.textAlign = 'center';
                 headerDiv.appendChild(headerH1);
                 header.appendChild(headerDiv);
                 form.appendChild(header);
+                /*---------------*/
 
+                /*button building*/
                 button.setAttribute('type', 'button');
                 button.setAttribute('value', 'Check answers');
                 form.appendChild(button);
+                /*---------------*/
+
                 anchor.appendChild(form);
 
-                for (var i = 0; i < this.data.questions.length; i++) {
-                    form.insertBefore(this.qCreator(
-                        this.data.questions[0].title,
-                        this.data.questions[0].answers[0],
-                        i
-                    ), form.children[i+1]);
+                for (var i = 0; i< this.data.questions.length; i++){
+                form.insertBefore(this.qCreator(i),form.children[i+1]);
                 }
-
             },
-            qCreator: function (question, answerOption, iValue) {
+            qCreator: function (ir) {
                 var section = this.gimmeBlock('section', 'row'),
                     sectionDiv = this.gimmeBlock('div', 'col-lg-4'),
                     sectionH2 = this.gimmeBlock('h2'),
-                    sectionUl = this.gimmeBlock('ul'),
-                    sectionLi = this.gimmeBlock('li'),
-                    sectionLabel = this.gimmeBlock('label'),
-                    sectionInput = this.gimmeBlock('input');
+                    H2text = document.createTextNode(this.data.questions[ir].title),
+                    sectionUl = this.gimmeBlock('ul');
 
-                sectionH2.appendChild(document.createTextNode(question));
+                sectionH2.appendChild(H2text);
                 sectionDiv.appendChild(sectionH2);
                 section.appendChild(sectionDiv);
-                sectionInput.setAttribute('type', 'checkbox');
-                sectionLabel.appendChild(sectionInput);
-                sectionLabel.appendChild(document.createTextNode(answerOption));
-                sectionLi.appendChild(sectionLabel);
                 sectionUl.style.listStyleType = 'none';
-                sectionUl.appendChild(sectionLi);
-
-                for (var i = 0; i < (this.data.questions[iValue].answers.length) - 1; i++) {
-                    sectionUl.appendChild(sectionLi.cloneNode(true));
-                }
-
                 sectionDiv.appendChild(sectionUl);
-                return section;
-            }
 
+                for (var i = 0; i < this.data.questions[ir].answers.length; i++) {
+                    var
+                        sectionLi = this.gimmeBlock('li'),
+                        sectionLabel = this.gimmeBlock('label'),
+                        sectionInput = this.gimmeBlock('input'),
+                        labelText = document.createTextNode(this.data.questions[ir].answers[i]);
+
+                    sectionInput.setAttribute('type', 'checkbox');
+                    sectionLabel.appendChild(sectionInput);
+                    sectionLabel.appendChild(labelText);
+                    sectionLi.appendChild(sectionLabel);
+                    sectionUl.appendChild(sectionLi);
+                    console.log(this.data.questions[ir].answers.length);
+                }
+                return section;
+            },
+
+
+            init: function () {
+                this.form();
+            }
         };
-    test.form();
+    test.init();
 })();
